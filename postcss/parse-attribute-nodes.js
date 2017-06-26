04143a995854b5463f3a5a3bf8516054be17efc7
iddan <mail@aniddan.com>
2017-06-27 01:09:10 +0300

    Using hash to identify attribute selectors

--- postcss/parse-attribute-nodes.js
+++ postcss/parse-attribute-nodes.js
@@ -1,10 +1,11 @@
 const parser = require('postcss-selector-parser');
-const shortid = require('shortid');
+const { unique } = require('shorthash');
 
 const parseAttributeNodes = (id, componentName, nodes) =>
   nodes.map(node => {
     const { operator, attribute, raws: { unquoted, insensitive }} = node;
-    const attributeClassName = `${ componentName }-${ attribute }_${ shortid.generate() }_${ id }`;
+    const attributeId = operator ? unique(operator + unquoted) : '';
+    const attributeClassName = `${ componentName }-${ attribute }_${ attributeId }_${ id }`;
     node.replaceWith(parser.className({ value: attributeClassName }));
     return {
       operator,
